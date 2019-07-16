f05dbde73fcea77a234b4a4460746041d9940a0e
Geon._.Uk <30423696+parkgunuk@users.noreply.github.com>
2019-07-16 09:44:53 +0900

    Create template.vue

---
+++ frontend/src/template.vue
@@ -0,0 +1,30 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: test,
+	props: {
+		
+	},
+	data() {
+		return {
+        }
+    },
+	components: {
+		
+	},
+	mounted() {
+	
+	},
+	methods: {
+		
+	}
+
+}
+</script>
+
+<style>
+
+</style>
