003479d78cc15233de023f762f15f7590a35dd17
GeonUk <zzaing0502@gmail.com>
2019-07-17 17:45:04 +0900

    homopage 진행중

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <Header></Header>
     <router-view/>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Header from "./views/Header";
 import Footer from "./views/Footer";
 
 export default {
   name: 'App',
   components: {
-    Header,
     Footer
   }
 }
