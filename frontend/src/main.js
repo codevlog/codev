15ec5c7010abb2d8ea39b0466dcdad01aaf3a132
GeonUk <zzaing0502@gmail.com>
2019-07-18 17:27:33 +0900

    hompage 90퍼 완성(S1P1112027-52)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './config/router'
 import store from './vuex/store'
 
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
